b3c99a0f3a1d21d6c2dcd68f5477c868bd977b35
CesarBrunoM <58617507+CesarBrunoM@users.noreply.github.com>
2022-04-24 22:18:46 -0300

    Funções Usuário
    Inicio da configuração de usuário logado  e função logout definida.

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -2,9 +2,10 @@ from flask import render_template, request, flash, url_for, redirect
 from comunidadeimpressionadora import app, database, bcrypt
 from comunidadeimpressionadora.forms import FormLogin, FormCriarConta
 from comunidadeimpressionadora.models import Usuarios
-from flask_login import login_user
+from flask_login import login_user, logout_user, current_user
 
 lista_usuarios = ['Bruno', 'Luana', 'Lucas', 'Divino']
+
 
 @app.route('/')
 def home():
@@ -36,7 +37,6 @@ def login():
         # sucesso no login, levando em consideração o botão clicado
 
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
-
         senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
         usuario = Usuarios(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
         database.session.add(usuario)
@@ -46,3 +46,19 @@ def login():
         return redirect(url_for('home'))
         # conta criada com sucesso, levando em consideração o botão clicado
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+
+
+@app.route('/sair')
+def sair():
+    logout_user()
+    flash(f'Logout feito com sucesso', 'alert-success')
+    return redirect(url_for('home'))
+
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
+@app.route('/post/criar')
+def criar_post():
+    return render_template('criarpost.html')
